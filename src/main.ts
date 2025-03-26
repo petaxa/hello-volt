49a333551cbb5bf1209799bd7d2d3e8fa0293f50
tempestif <soccer.i_y@icloud.com>
2025-03-27 03:48:30 +0900

    configure primevue

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import PrimeVue from "primevue/config";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(PrimeVue, {
+  unstyled: true,
+});
+
+app.mount("#app");
